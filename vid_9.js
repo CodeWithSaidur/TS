e219681aabcf4274a721794d0e9260b58a2c5df7
saidur Rohman Barbhuiya <132422574+sabed2023@users.noreply.github.com>
2025-09-03 20:54:29 +0530

    vid 9

---
+++ vid_9.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// Array
+var arr = [];
+arr.push('Hello World!');
+// console.log(arr)
+var arr2 = [];
+arr2.push(1);
+arr2.push(1);
+arr2.push(1);
+console.log(arr2);
---
+++ vid_9.ts
@@ -0,0 +1,13 @@
+// Array
+let arr: string[] = []
+arr.push('Hello World!')
+// console.log(arr)
+
+let arr2: Array<number> = []
+arr2.push(1)
+arr2.push(2)
+arr2.push(3)
+// console.log(arr2)
+
+
+export {}
