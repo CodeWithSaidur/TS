d989c40bcedc63d7a324ba148072584da76e7296
saidur Rohman Barbhuiya <132422574+sabed2023@users.noreply.github.com>
2025-09-18 19:11:21 +0530

    vid_16

--- vid_16.js
+++ vid_16.js
@@ -1,6 +1,6 @@
 "use strict";
-// 02:54:00
 Object.defineProperty(exports, "__esModule", { value: true });
+// 02:54:00
 var User = /** @class */ (function () {
     function User(id, name, email) {
         this.id = id;
@@ -11,13 +11,3 @@ var User = /** @class */ (function () {
 }());
 var u1 = new User(1, 'sabed', 'H5cY3@example.com');
 console.log(u1.name);
-var Admin = /** @class */ (function () {
-    function Admin(id, name, email) {
-        id = id;
-        name = name;
-        email = email;
-    }
-    return Admin;
-}());
-var a1 = new Admin(1, 'sabed', 'H5cY3@example.com');
-console.log(a1);
